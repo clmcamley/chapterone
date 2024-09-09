f379d32720adf73e1dec49b9e5a01bc706425f09
caitmann <caitlynleemann@gmail.com>
2024-09-09 14:29:01 -0400

    first commit

---
+++ src/Onedottwo.java
@@ -0,0 +1,9 @@
+public class Onedottwo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
